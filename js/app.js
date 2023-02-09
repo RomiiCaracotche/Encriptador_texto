3e159db8a71e6337ce9ffc1f690458ef5d1f0d62
romi <rocaracotche@gmail.com>
2023-02-09 15:21:59 -0300

    errores boton copiar corregido

--- js/app.js
+++ js/app.js
@@ -8,6 +8,10 @@ let copiar = document.querySelector('.copiar');
 let encriptar = document.querySelector('.boton-encriptar');
 let desencriptar = document.querySelector('.boton-desencriptar');
 
+const figura = document.querySelector('.figura');
+const mensaje = document.querySelector('.mensaje');
+const alerta = document.querySelector('.alerta');
+
 window.addEventListener('DOMContentLoaded', (e) => {
     copiar.style.display = "none";
 });
@@ -15,14 +19,14 @@ window.addEventListener('DOMContentLoaded', (e) => {
 encriptar.addEventListener('click', (e) => {
     let textoDesencriptado = document.querySelector('.texto').value.toLowerCase();
     let texto = textoDesencriptado.replace(/e/mgi , "enter").replace(/o/mgi , "ober").replace(/i/mgi , "imes").replace(/a/mgi , "ai").replace(/u/mgi , "ufat");
-    const figura =  document.querySelector('.figura').style.display = "none";
-    const alerta =  document.querySelector('.alerta').style.display = "none";
-    document.querySelector('.mensaje').innerHTML = texto;
+    figura.style.display = "none";
+    alerta.style.display = "none";
+    mensaje.innerHTML = texto;
     copiar.style.display = "initial"; 
     if (screen.width > 768) {
         //pantalla grande
-        document.querySelector('.mensaje').style.marginTop = "25px";
-        document.querySelector('.mensaje').style.height = "75%";
+        mensaje.style.marginTop = "25px";
+        mensaje.style.height = "75%";
         copiar.style.marginBottom = "25px";
         document.querySelector('.caja-der').style.justifyContent = "space-between";
     }
@@ -43,12 +47,13 @@ encriptar.addEventListener('click', (e) => {
 desencriptar.addEventListener('click', (e) => {
     let textoEncriptado = document.querySelector('.texto').value.toLowerCase();
     let texto = textoEncriptado.replace(/enter/mgi , "e").replace(/ober/mgi , "o").replace(/imes/mgi , "i").replace(/ai/mgi , "a").replace(/ufat/mgi , "u");
-    const figura =  document.querySelector('.figura').style.display = "none";
-    const alerta =  document.querySelector('.alerta').style.display = "none";
-    document.querySelector('.mensaje').innerHTML = texto;
+    figura.style.display = "none";
+    alerta.style.display = "none";
+    mensaje.innerHTML = texto;
+    mensaje.style.marginTop = "25px";
     document.querySelector('.caja-der').style.justifyContent = "space-between";
-    document.querySelector('.mensaje').style.marginTop = "25px";
-    document.querySelector('.copiar').style.marginBottom = "25px";
+    copiar.style.display = "initial"; 
+    copiar.style.marginBottom = "25px";
 });
 
 copiar.addEventListener('click', (e) => {
