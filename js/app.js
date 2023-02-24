c1221a9b1645efc6cb39416c9c87751ab4c4c92a
romi <rocaracotche@gmail.com>
2023-02-24 13:54:18 -0300

    mejoras visuales

--- js/app.js
+++ js/app.js
@@ -4,61 +4,70 @@ i --> imes
 a --> ai
 u --> ufat */
 
-let copiar = document.querySelector('.copiar');
-let encriptar = document.querySelector('.boton-encriptar');
-let desencriptar = document.querySelector('.boton-desencriptar');
+let copiar = document.querySelector('.contenedor-secun-der__copiar');
+let encriptar = document.querySelector('.contenedor-botones__encriptar');
+let desencriptar = document.querySelector('.contenedor-botones__desencriptar');
 
-const figura = document.querySelector('.figura');
-const mensaje = document.querySelector('.mensaje');
-const alerta = document.querySelector('.alerta');
+const figura = document.querySelector('.contenedor-secun-der__figura');
+const mensaje = document.querySelector('.contenedor-secun-der__mensaje');
+const alerta = document.querySelector('.contenedor-secun-der__alerta');
 
 window.addEventListener('DOMContentLoaded', (e) => {
     copiar.style.display = "none";
 });
 
 encriptar.addEventListener('click', (e) => {
-    let textoDesencriptado = document.querySelector('.texto').value.toLowerCase();
-    let texto = textoDesencriptado.replace(/e/mgi , "enter").replace(/o/mgi , "ober").replace(/i/mgi , "imes").replace(/a/mgi , "ai").replace(/u/mgi , "ufat");
-    figura.style.display = "none";
-    alerta.style.display = "none";
-    mensaje.innerHTML = texto;
-    copiar.style.display = "initial"; 
-    if (screen.width > 768) {
-        //pantalla grande
-        mensaje.style.marginTop = "25px";
-        mensaje.style.height = "75%";
-        copiar.style.marginBottom = "25px";
-        document.querySelector('.caja-der').style.justifyContent = "space-between";
+    let texto = document.querySelector('.contenedor-secun-izq__texto').value.toLowerCase();
+    if(texto == ""){
+        alert("Debe escribir para poder encriptar...");
     }
     else {
-        if((screen.width > 375) && (screen.width < 768)) {
-            console.log("tablet");
-            //tablet
+        let textoEncriptado = texto.replace(/e/mgi , "enter").replace(/o/mgi , "ober").replace(/i/mgi , "imes").replace(/a/mgi , "ai").replace(/u/mgi , "ufat");
+        mensaje.innerHTML = textoEncriptado;
+        figura.style.display = "none";
+        alerta.style.display = "none";
+        copiar.style.display = "initial"; 
+        /*if (screen.width > 768) {
+            //pantalla grande
+            console.log("pc");
+            mensaje.style.height = "75%";
+            document.querySelector('.contenedor-secun-der').style.justifyContent = "space-between";
         }
         else {
-            console.log("celular");
-            //celular
-        }
+            if((screen.width > 375) && (screen.width < 768)) {
+                console.log("tablet");
+                //tablet
+            }
+            else {
+                console.log("celular");
+                //celular
+            }
+        }*/
     }
-    
- 
 });
 
 desencriptar.addEventListener('click', (e) => {
-    let textoEncriptado = document.querySelector('.texto').value.toLowerCase();
-    let texto = textoEncriptado.replace(/enter/mgi , "e").replace(/ober/mgi , "o").replace(/imes/mgi , "i").replace(/ai/mgi , "a").replace(/ufat/mgi , "u");
-    figura.style.display = "none";
-    alerta.style.display = "none";
-    mensaje.innerHTML = texto;
-    mensaje.style.marginTop = "25px";
-    document.querySelector('.caja-der').style.justifyContent = "space-between";
-    copiar.style.display = "initial"; 
-    copiar.style.marginBottom = "25px";
+    let texto = document.querySelector('.contenedor-secun-izq__texto').value.toLowerCase();
+    if(texto == ""){
+        alert("Debe escribir para poder desencriptar...");
+    }
+    else {
+        let textoDesencriptado = texto.replace(/enter/mgi , "e").replace(/ober/mgi , "o").replace(/imes/mgi , "i").replace(/ai/mgi , "a").replace(/ufat/mgi , "u");
+        mensaje.innerHTML = textoDesencriptado;
+        figura.style.display = "none";
+        alerta.style.display = "none";
+        copiar.style.display = "initial";
+
+    }
 });
 
 copiar.addEventListener('click', (e) => {
     let contenido = document.querySelector('#contenido');
     contenido.select();
     document.execCommand("copy");
+    const temp = setTimeout(function(){
+        document.querySelector('.contenedor-secun-der__copiar').innerHTML = "Copiar";
+    }, 4000); 
+    document.querySelector('.contenedor-secun-der__copiar').innerHTML = "Copiado!";
 });
 
